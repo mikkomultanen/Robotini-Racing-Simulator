2974e0959c3ba291246be10538905011a2f16928
Juha Paananen <juha.paananen@gmail.com>
2019-05-03 21:08:13 +0300

    continuously generate screenshots to screenshot.png

---
+++ Assets/Scripts/CameraOutputController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOutputController : MonoBehaviour
+{
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int width = 128;
+        int height = 80;
+
+        camera.aspect = 1.0f;
+        // recall that the height is now the "actual" size from now on
+
+        RenderTexture tempRT = new RenderTexture(width, height, 24);
+        // the 24 can be 0,16,24, formats like
+        // RenderTextureFormat.Default, ARGB32 etc.
+
+        camera.targetTexture = tempRT;
+        camera.Render();
+
+        RenderTexture.active = tempRT;
+        Texture2D virtualPhoto =
+            new Texture2D(width, height, TextureFormat.RGB24, false);
+        // false, meaning no need for mipmaps
+        virtualPhoto.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+
+        RenderTexture.active = null; //can help avoid errors 
+        camera.targetTexture = null;
+        // consider ... Destroy(tempRT);
+
+        byte[] bytes;
+        bytes = virtualPhoto.EncodeToPNG();
+
+        System.IO.File.WriteAllBytes("./screenshot.png", bytes);
+        // virtualCam.SetActive(false); ... no great need for this.
+    }
+}
