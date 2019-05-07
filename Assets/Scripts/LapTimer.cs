24c698d769e38a06a9014257a7d6d0716e5ee688
McDevon <jussi.enroos@gmail.com>
2019-05-07 22:28:45 +0300

    Add Text Mesh Pro and first version of timer UI

--- Assets/Scripts/LapTimer.cs
+++ Assets/Scripts/LapTimer.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LapTimer : MonoBehaviour
 {
     private Dictionary<string, TimeWrapper> timers = new Dictionary<string, TimeWrapper>();
+
+    [SerializeField] private GameObject lapTimeList;
+    [SerializeField] private GameObject lapTimeRowPrefab;
+
+    public void ToggleLapTimeList()
+    {
+        Debug.Log("TAP");
+        lapTimeList?.SetActive(!lapTimeList.activeSelf);
+    }
 
     public void CarHitTrigger(GameObject carObject)
     {
         if (carObject.GetComponent<CarController>())
         {
             string carName = carObject.name;
-            Debug.Log("Car hit trigger: " + carName);
 
             if (timers.ContainsKey(carName))
             {
+                if (!timers[carName].timeListElement)
+                {
+                    GameObject row = Instantiate(lapTimeRowPrefab);
+                    timers[carName].timeListElement = row;
+                    row.transform.parent = lapTimeList.transform;
+                    RectTransform rect = row.GetComponent<RectTransform>();
+                    rect.localScale = Vector3.one;
+                    rect.anchoredPosition = new Vector2(0, -25);
+                }
+                timers[carName].NewLapTime();
                 NewLapTime(carName, timers[carName]);
-                timers[carName].time = 0;
             }
             else
             {
+                Debug.Log("New car entering race: " + carName);
                 timers[carName] = new TimeWrapper();
             }
         }
@@ -28,12 +47,6 @@ public class LapTimer : MonoBehaviour
     private void NewLapTime(string carName, TimeWrapper timeWrapper)
     {
         Debug.Log("Lap time for " + carName + ": " + timeWrapper.time);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     void FixedUpdate()
@@ -48,5 +61,35 @@ public class LapTimer : MonoBehaviour
     private class TimeWrapper
     {
         internal float time;
+        internal float bestTime = float.MaxValue;
+        internal float lastTime = 0;
+        internal int lapCount;
+
+        internal GameObject timeListElement;
+
+        internal void NewLapTime()
+        {
+            lapCount++;
+            lastTime = time;
+            if (time < bestTime)
+            {
+                bestTime = time;
+            }
+            time = 0;
+
+            if (timeListElement)
+            {
+                timeListElement.transform.Find("LastLap").GetComponent<TextMeshProUGUI>().text = FormattedTime(lastTime);
+                timeListElement.transform.Find("BestLap").GetComponent<TextMeshProUGUI>().text = FormattedTime(bestTime);
+                timeListElement.transform.Find("LapCount").GetComponent<TextMeshProUGUI>().text = lapCount.ToString();
+            }
+        }
+
+        private string FormattedTime(float inputTime)
+        {
+            int minutes = (int)(inputTime / 60);
+            int seconds = (int)(inputTime) % 60;
+            return string.Format("{0:00}:{1:00}{2:.000}", minutes, seconds, inputTime - Mathf.Floor(inputTime));
+        }
     }
 }
