9e55a2807708c3ac2e4700ec65153394759294ee
McDevon <jussi.enroos@gmail.com>
2019-05-07 21:21:08 +0300

    Add working lap timer

---
+++ Assets/Scripts/TriggerEventSource.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnCollisionEvent : UnityEvent<GameObject> { }
+
+public class TriggerEventSource : MonoBehaviour
+{
+    [SerializeField] private OnCollisionEvent collisionEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collisionEvent.Invoke(other.gameObject);
+    }
+}
