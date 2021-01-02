c1512e1a958aaa657fd0b6befce01dc77a14bb67
Juha Paananen <juha.paananen@gmail.com>
2021-01-02 19:41:59 +0200

    clean countdown

--- Assets/Scripts/CountdownUI.cs
+++ Assets/Scripts/CountdownUI.cs
@@ -6,6 +6,9 @@ public class CountdownUI : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        EventBus.Subscribe<ProceedToNextPhase>(this, s => {
+            gameObject.GetComponent<TextMeshProUGUI>().text = "";
+        });
         EventBus.Subscribe<SecondsRemaining>(this, s => {
             var txt = s.secondsRemaining > 0 ? "" + s.secondsRemaining : "";
             gameObject.GetComponent<TextMeshProUGUI>().text = txt;
