c8abb01a4ce72f78f8d3b551279bdf5e05fad82e
Juha Paananen <juha.paananen@gmail.com>
2021-02-22 12:35:11 +0200

    keep running in background

--- Assets/Scripts/ModeController.cs
+++ Assets/Scripts/ModeController.cs
@@ -39,10 +39,15 @@ public class ModeController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        Application.runInBackground = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!Application.runInBackground) {
+             Application.runInBackground = true;
+             Debug.Log("Re-Setting Application.runInBackground to TRUE at: " + Time.time);
+        }
     }
 }
