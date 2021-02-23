e1940075356b6a6d2d7934b8c278ea303e2a5d39
Mikko Väänäkoski <mikko.vaanakoski@reaktor.com>
2021-02-23 21:21:19 +0200

    Use "Double buffering" to stabilise reading camera data from the GPU

--- Assets/Scripts/CameraOutputController.cs
+++ Assets/Scripts/CameraOutputController.cs
@@ -7,17 +7,75 @@ using Unity.Collections;
 [RequireComponent(typeof(Camera))]
 public class CameraOutputController : MonoBehaviour
 {
+    private class GPUReader : IDisposable
+    {
+        private NativeArray<uint> outputArray;
+        private AsyncGPUReadbackRequest request;
+        private bool hasRequest = false;
+        private bool outputReady = false;
+
+        public GPUReader(int size)
+        {
+            outputArray = new NativeArray<uint>(size, Allocator.Persistent);
+        }
+
+        public void Read(RenderTexture renderTexture)
+        {
+            request = AsyncGPUReadback.RequestIntoNativeArray(ref outputArray, renderTexture, 0, TextureFormat.ARGB32, OnCompleteReadback);
+            hasRequest = true;
+        }
+
+
+        public bool WriteTo(Texture2D texture)
+        {
+            if (hasRequest)
+            {
+                request.WaitForCompletion();
+            }
+            if (outputReady)
+            {
+                outputReady = false;
+                texture.LoadRawTextureData(outputArray);
+                texture.Apply();
+                return true;
+            }
+            return false;
+        }
+
+        void OnCompleteReadback(AsyncGPUReadbackRequest request)
+        {
+            hasRequest = false;
+            if (request.hasError)
+            {
+                Debug.Log("GPU readback error detected.");
+                return;
+            }
+            outputReady = true;
+        }
+
+        public void Dispose()
+        {
+            if (hasRequest)
+            {
+                request.WaitForCompletion();
+            }
+            outputArray.Dispose();
+        }
+    }
+
+    private const int READERS_LENGTH = 2;
+    private const int CURRENT = 0;
+    private const int NEXT = READERS_LENGTH - 1;
+
     private Camera mCamera;
     public RenderTexture renderTexture;
-    private NativeArray<uint> outputArray;
-    private AsyncGPUReadbackRequest request;
-    private bool hasRequest = false;
-    private bool outputReady = false;
+    private GPUReader[] readers = new GPUReader[READERS_LENGTH];
     private Texture2D virtualPhoto;
     private float lastSaved = 0;
     private const int width = 128;
     private const int height = 80;
-    
+
+
     private volatile CarSocket socket;
 
     private void Start()
@@ -25,7 +83,10 @@ public class CameraOutputController : MonoBehaviour
         mCamera = GetComponent<Camera>();
         renderTexture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
         renderTexture.antiAliasing = 2;
-        outputArray = new NativeArray<uint>(width * height, Allocator.Persistent);
+        for (int i = 0; i < readers.Length; ++i)
+        {
+            readers[i] = new GPUReader(width * height);
+        }
         virtualPhoto = new Texture2D(width, height, TextureFormat.ARGB32, false);
         Debug.Log("CameraOutputController started");
         mCamera.rect = new Rect(0, 0, 1, 1);
@@ -38,45 +99,21 @@ public class CameraOutputController : MonoBehaviour
     void OnEndFrameRendering(ScriptableRenderContext context, Camera[] cameras)
     {
         if (socket == null) return;
-        if (Time.time < lastSaved + 0.03)
+        if (Time.time < lastSaved + 0.03 || socket.SendQueueSize() > 1)
         {
-            return;
-        }
-
-        if (hasRequest)
-        {
-            request.WaitForCompletion();
-        }
-        if (outputReady)
-        {
-            outputReady = false;
-            socket.Send(encodeFrame(virtualPhoto));
-        }
-
-        if (socket.SendQueueSize() > 1) {
             return;
         }
 
         lastSaved = Time.time;
 
-        request = AsyncGPUReadback.RequestIntoNativeArray(ref outputArray, renderTexture, 0, TextureFormat.ARGB32, OnCompleteReadback);
-        hasRequest = true;
-    }
+        readers[NEXT].Read(renderTexture);
 
-    void OnCompleteReadback(AsyncGPUReadbackRequest request)
-    {
-        hasRequest = false;
-        if (request.hasError)
+        if (readers[CURRENT].WriteTo(virtualPhoto))
         {
-            Debug.Log("GPU readback error detected.");
-            return;
+            socket.Send(encodeFrame(virtualPhoto));
         }
-        if (virtualPhoto == null || socket == null) {
-            return;
-        }
-        virtualPhoto.LoadRawTextureData(outputArray);
-        virtualPhoto.Apply();
-        outputReady = true;
+
+        Roll(readers);
     }
 
     private void OnDestroy()
@@ -85,11 +122,14 @@ public class CameraOutputController : MonoBehaviour
         {
             this.socket = null;
         }
-        if (hasRequest)
+        for (int i = 0; i < readers.Length; ++i)
         {
-            request.WaitForCompletion();
+            if (readers[i] != null)
+            {
+                readers[i].Dispose();
+            }
+            readers[i] = null;
         }
-        outputArray.Dispose();
     }
 
     private byte[] encodeFrame(Texture2D virtualPhoto)
@@ -109,5 +149,15 @@ public class CameraOutputController : MonoBehaviour
     {
         if (this.socket != null) return;
         this.socket = socket;
+    }
+
+    private static void Roll(GPUReader[] array)
+    {
+        GPUReader tmp = array[0];
+        for (int i = 1; i < array.Length; ++i)
+        {
+            array[i - 1] = array[i];
+        }
+        array[array.Length - 1] = tmp;
     }
 }
