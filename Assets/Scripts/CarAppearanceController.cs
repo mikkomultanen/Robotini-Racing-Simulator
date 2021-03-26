58de8a10d56fb00443889a155c10d7f579e92523
Mikko Väänäkoski <mikko.vaanakoski@reaktor.com>
2021-03-26 13:34:34 +0200

    Apply car texture also to tires

--- Assets/Scripts/CarAppearanceController.cs
+++ Assets/Scripts/CarAppearanceController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CarAppearanceController : MonoBehaviour
 {
-    public MeshRenderer bodyRenderer;   
+    public MeshRenderer bodyRenderer;
+    public MeshRenderer[] tireRenderers;
+    [NonSerialized]
     public CarInfo CarInfo;
 
     private void Start()
@@ -29,8 +32,14 @@ public class CarAppearanceController : MonoBehaviour
         if (texture != null)
         {
             Debug.Log("Loaded car texture for " + CarInfo.teamId);
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Bilinear;
             bodyRenderer.material.color = Color.white;
             bodyRenderer.material.mainTexture = texture;
+            foreach (var tireRenderer in tireRenderers) {
+                tireRenderer.material.color = Color.white;
+                tireRenderer.material.mainTexture = texture;
+            }
         }
         else {
             Debug.Log("Failed to load car texture for " + CarInfo.teamId + ": " + www.error);
